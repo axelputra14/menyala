cd9402803e6e33f094e9635930a90b49e16d236b
Octavian Axel Putra <octavian.axel.putra@gmail.com>
2025-09-16 21:30:17 +0700

    tray and single instance

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,7 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use tauri::tray::TrayIconBuilder;
+use tauri::{AppHandle, Manager};
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
@@ -6,7 +9,22 @@ fn greet(name: &str) -> String {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    let mut builder = tauri::Builder::default();
     tauri::Builder::default()
+        .setup(|app| {
+            let tray = TrayIconBuilder::new()
+            .icon(app.default_window_icon().unwrap().clone())
+            .build(app)?;
+            #[cfg(desktop)]
+                {
+                    builder = builder.plugin(tauri_plugin_single_instance::init(|app, args, cwd| {
+                    let _ = app.get_webview_window("main")
+                       .expect("no main window")
+                       .set_focus();
+                }));
+            }
+            Ok(())
+        })
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
